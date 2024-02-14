d4ddf87b084ddcee3e3fddb01c64a835215d1d0f
Nina Finageeva <finageeva.nina@mail.ru>
2024-02-14 16:33:20 +0300

    working on code in homework-3

--- src/channel.py
+++ src/channel.py
@@ -23,6 +23,43 @@ class Channel:
         self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
         self.video_count = self.channel['items'][0]['statistics']['videoCount']
         self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    def __str__(self):
+        """__str__
+        +
+        """
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        """
+        -
+        """
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        """
+        >
+        """
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        """
+        >=
+        """
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __lt__(self, other):
+        """
+        <
+        """
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        """
+        <=
+        """
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         pprint(json.dumps(self.channel, indent=2, ensure_ascii=False))
@@ -45,9 +82,10 @@ class Channel:
     def channel_id(self):
         return self.__channel_id
 
-    @channel_id.setter
-    def channel_id(self, value):
-        self.__channel_id = value
+
+    # @channel_id.setter
+    # def channel_id(self, value):
+    #     self.__channel_id = value
 
     # @property
     # def title(self):
